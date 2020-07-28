27e073a54df664e5b9b3e875fc8534980ea9f67e
Paweł Gajda <pawegio@gmail.com>
2020-07-28 15:37:26 +0200

    Downgrade compose to dev14 (with working layout preview)

--- app/src/main/java/pl/elpassion/composeplayground/MainActivity.kt
+++ app/src/main/java/pl/elpassion/composeplayground/MainActivity.kt
@@ -5,34 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
-import androidx.ui.material.MaterialTheme
-import androidx.ui.material.Surface
 import androidx.ui.tooling.preview.Preview
-import pl.elpassion.composeplayground.ui.ComposePlaygroundTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text("Hello World!")
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposePlaygroundTheme {
-        Greeting("Android")
-    }
+    Text("Hello World!")
 }
